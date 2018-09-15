33e247b6abf073befe91ab15c3236f6ea4960f39
Oleksandr Miroshnychenko <almir.php.developer@gmail.com>
2018-09-15 14:00:52 +0300

    update_1

--- resources/views/layouts/navigate.blade.php
+++ resources/views/layouts/navigate.blade.php
@@ -1,25 +1,26 @@
-<div class="collapse navbar-collapse scrollspy" id="navbar-collapse-1">
+<div class="collapse navbar-collapse" id="navbar-collapse-1">
     <!-- main-menu -->
-    <ul class="navbar-nav ml-lg-auto">
+    <ul class="navbar-nav ml-xl-auto">
         @foreach($pages as $page)
             @if($page->route == "uslugi")
-            <li class="nav-item dropdown">
-                <a href="{{ $page->route }}" class="nav-link dropdown-toggle" id="{{ $page->route }}" aria-haspopup="true" aria-expanded="false" >
-                    {{ $page->name }}
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="{{ $page->route }}">
-                    @foreach($services as $service)
-                    <li><a href="{{ $service->route }}">{{ $service->title }}</a></li>
-                    @endforeach
-                </ul>
+                <li class="nav-item dropdown">
+                    <a href="{{ $page->route }}" class="nav-link dropdown-toggle"
+                       aria-haspopup="true" aria-expanded="false">
+                        {{ $page->name }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="{{ $page->route }}">
+                        @foreach($services as $service)
+                            <li><a href="{{ $service->route }}">{{ $service->title }}</a></li>
+                        @endforeach
+                    </ul>
 
-            </li>
+                </li>
             @else
-                        <li class="nav-item">
-                            <a href="{{ $page->route ?: url("/") }}" class="nav-link">
-                                {{ $page->name }}
-                            </a>
-                        </li>
+                <li class="nav-item">
+                    <a href="{{ $page->route ?: url("/") }}" class="nav-link">
+                        {{ $page->name }}
+                    </a>
+                </li>
             @endif
         @endforeach
     </ul>
